c60da1f5bf1eb24c0e251c0e6d46083b7af6d85d
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-03-04 16:39:58 +0000

    Added database connection

--- config.js
+++ config.js
@@ -4,5 +4,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const PUBLIC_PATH = process.env.PUBLIC_PATH || `${__dirname}/public`;
+export const PUBLIC_PATH = `${__dirname}/public`;
 export const PORT = process.env.PORT || 2000;
+export const MONGO_USER = process.env.MONGO_USER;
+export const MONGO_PASS = process.env.MONGO_PASS;
+export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+export const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster1.oaorkkq.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
+
 import userRouter from "./routes/user.router.js";
 import authRouter from "./routes/auth.router.js";
 import quoteRouter from "./routes/quote.router.js";
-import { PUBLIC_PATH, PORT } from "./config.js";
+import { PUBLIC_PATH, PORT, MONGO_URI } from "./config.js";
 
 // Create express app with HTTP server
 const app = express();
@@ -22,6 +24,9 @@ app.use("/quotes", quoteRouter);
 
 // Page not found - standard redirect
 app.use((req, res) => res.redirect("/"));
+
+// Connect to database
+await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
 
 // Start server listening for requests
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
