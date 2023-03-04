341113f5e2f4ade61acbf55af05fedd252938e45
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-03-04 16:48:20 +0000

    Moved secrets to .env file

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
+import * as dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 export const PUBLIC_PATH = `${__dirname}/public`;
-export const PORT = process.env.PORT || 2000;
+
+export const PORT = process.env.PORT;
+
 export const MONGO_USER = process.env.MONGO_USER;
 export const MONGO_PASS = process.env.MONGO_PASS;
 export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
