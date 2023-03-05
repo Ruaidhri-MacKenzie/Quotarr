6176bff81a92dbd45ebf33ca4df43127b8d0d8ea
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-03-05 20:55:04 +0000

    Removed task schema

--- models/quote.js
+++ models/quote.js
@@ -2,7 +2,16 @@ import mongoose from "mongoose";
 
 const QuoteSchema = new mongoose.Schema({
 	_id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-	tasks: [{ type : mongoose.Schema.Types.ObjectId, ref: "Task" }],
+	tasks: [{
+		juniorHours: { type: Number, default: 0 },
+		seniorHours: { type: Number, default: 0 },
+		expertHours: { type: Number, default: 0 },
+		items: [{
+			name: { type: String, default: "" },
+			cost: { type: Number, default: 0, min: 0 },
+			quantity: { type: Number, default: 1, min: 1 },
+		}],
+	}],
 	fudgeFactor: { type: Number, default: 1.1 },
 	createTime: { type: Date, default: Date.now },
 });
