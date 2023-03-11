c232aee61351ce470b9c9e31dfc013dd9d621fe6
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-03-11 00:03:59 +0000

    Added total and subtotals to quotes

--- client/src/components/QuoteView/QuoteView.js
+++ client/src/components/QuoteView/QuoteView.js
@@ -1,10 +1,38 @@
+import { useState, useEffect } from "react";
+import TaskView from "../TaskView/TaskView";
 import "./QuoteView.css";
 
 const QuoteView = ({ quote }) => {
+	const [total, setTotal] = useState(0);
+	
+	useEffect(() => {
+		if (quote.tasks) {
+			setTotal(quote.tasks.reduce((total, task) => {
+				return total += task.labourCost + task.items.reduce((subtotal, line) => {
+					return subtotal += (line.cost * line.quantity);
+				}, 0);
+			}, 0));
+		}
+	}, [quote]);
+
+	if (!quote.name) {
+		return (
+		<div className="quote-view">
+			<p className="quote-view__empty">No Quote Selected</p>
+		</div>
+		);
+	}
+
 	return (
 		<div className="quote-view">
-			{quote && <p className="quote-view__name">{quote.name}</p>}
-			{!quote && <p className="quote-view__empty">No Quote</p>}
+			<header className="quote-view__header">
+				<h2 className="quote-view__name">{quote?.name || "Quote"}</h2>
+				<p className="quote-view__total">Total: £{total.toFixed(2)}</p>
+			</header>
+			<div className="quote-view__tasks">
+				{quote.tasks && quote.tasks.map((task, index) => <TaskView key={(task.name || "") + index} task={task} index={index} newQuote={false} />)}
+				{(!quote.tasks || quote.tasks.length === 0) && <p className="quote-view__tasks-empty">No Tasks</p>}
+			</div>
 		</div>
 	);
 };
--- client/src/components/TaskView/TaskView.js
+++ client/src/components/TaskView/TaskView.js
@@ -2,15 +2,24 @@ import LabourList from "../LabourList/LabourList";
 import ItemsList from "../ItemsList/ItemsList";
 import "./TaskView.css";
 
-const TaskView = ({ task, index }) => {
+const TaskView = ({ task, index, newQuote }) => {
+	const total = (!newQuote) ? task.labourCost + task.items.reduce((acc, cur) => acc += (cur.cost * cur.quantity), 0) : 0;
+
 	return (
 		<div className="task-view">
 			<header className="task-view__header">
 				<h3 className="task-view__name">Task {index + 1}: {task.name}</h3>
-				<button className="task-view__remove">&times;</button>
+				{newQuote && <button className="task-view__remove">&times;</button>}
+				{!newQuote && <p className="task-view__subtotal">Subtotal: £{total.toFixed(2)}</p>}
 			</header>
-			<LabourList labour={task.labour} />
+			{newQuote && <LabourList labour={task.labour} />}
 			<ItemsList items={task.items} />
+			{!newQuote && (
+				<div className="task-view__labour">
+					<p className="task-view__labour-label">Labour</p>
+					<p className="task-view__labour-cost">£{task.labourCost.toFixed(2)}</p>
+				</div>
+			)}
 		</div>
 	);
 };
