f0fd6e4d8fc9faa041923eefe6e139bf9937ca8c
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-03-11 00:04:29 +0000

    Added conditional styling for selected quote

--- client/src/components/QuoteList/QuoteList.js
+++ client/src/components/QuoteList/QuoteList.js
@@ -1,22 +1,20 @@
 import "./QuoteList.css";
 
-const QuoteItem = ({ quote, setSelected }) => {
-	const handleClick = (event) => {
-		setSelected(quote);
+const QuoteItem = ({ quote, index, selected, setSelected }) => {
+	const selectQuote = (event) => {
+		setSelected(current => (current?.name === quote.name) ? null : quote);
 	};
-
+	
 	return (
-		<li className="quote-list-item" onClick={handleClick}>
-			<p className="quote-list-item__name">{quote.name}</p>
-		</li>
+		<li className={"quote-list-item" + ((selected?.name === quote.name) ? " quote-list-item--selected" : "")} tabIndex="0" onClick={selectQuote}>Quote {index + 1}: {quote.name}</li>
 	);
 };
 
-const QuoteList = ({ quotes, setSelected }) => {
+const QuoteList = ({ quotes, selected, setSelected }) => {
 	return (
 		<ul className="quote-list">
-			{quotes && quotes.map((quote, index) => <QuoteItem key={quote.name + index} quote={quote} setSelected={setSelected} />)}
-			{(!quotes || !quotes.length) && <p className="quote-list__empty">No Quotes</p>}
+			{quotes && quotes.map((quote, index) => <QuoteItem key={(quote.name || "") + index} quote={quote} index={index} selected={selected} setSelected={setSelected} />)}
+			{(!quotes || !quotes.length) && <li className="quote-list__empty">No Quotes</li>}
 		</ul>
 	);
 };
