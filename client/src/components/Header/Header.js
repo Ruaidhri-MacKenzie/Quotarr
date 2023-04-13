f35fbcaf4257575b54e7c57d526fdd3489f691d0
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2023-04-13 09:49:14 +0100

    Added username to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = ({ title, user, showAdmin, setShowAdmin, signOut }) => {
 	return (
 		<header className="header">
 			<h1 className="header__title">{title}</h1>
+			{user && <h2 className="header__username">{user.username}</h2>}
 			{user?.admin && <button className="header__admin" onClick={toggleShowAdmin}>{showAdmin ? "Home" : "Admin"}</button>}
 			{user && <button className="header__sign-out" onClick={signOut}>Sign Out</button>}
 		</header>
